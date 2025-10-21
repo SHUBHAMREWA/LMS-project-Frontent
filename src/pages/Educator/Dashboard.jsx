3c20f5da4a3b7216027555fdef8144edd90301d2
shubham <shubhamrewamp17@gmail.com>
2025-10-21 23:37:55 +0530

    some work did

--- src/pages/Educator/Dashboard.jsx
+++ src/pages/Educator/Dashboard.jsx
@@ -1,8 +1,40 @@
 import React from 'react'
 
+import  image from "../../assets/empty.jpg"
+
 const Dashboard = () => {
+
+
+
   return (
-    <div>Dashboard</div>
+    <div className='w-full min-h-screen bg-[#f2f4f5]'>   
+
+
+          <div className='w-full h-full  '> 
+
+
+               {/* show educatro Details */}
+                 <div className='flex flex-col lg:flex-row bg-white px-15 gap-3 lg:justify '>
+                 
+                  <div className=''>
+                       <img src={image} alt="" className='h-25 w-25 rounded-full p-2 border-4 border-black' /> 
+                  </div>
+                  
+ 
+                   <div className='flex flex-col gap-2'>                       
+                       <p>hellow india</p>
+                      <h1>kya bol riya</h1>
+
+                   </div>
+
+                 </div>
+              
+             
+
+
+          </div>
+
+    </div>
   )
 }
 
